b5d12e3cae2501e94dbfc754ba0103c2f55541e6
3ddelano <3ddelano@gmail.com>
2022-10-13 11:39:29 +0530

    feat: add stop command

--- server.js
+++ server.js
@@ -2,99 +2,131 @@ const fs = require("fs");
 const moment = require("moment");
 
 const config = JSON.parse(fs.readFileSync("./config.json"));
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
 let MESSAGE;
 
 // load enabled components
-const component_names = fs.readdirSync('./components/');
+const componentNames = fs.readdirSync("./components/");
 const components = new Map();
-component_names.forEach(file_name => {
-	if (!file_name.endsWith('.js')) return;
+componentNames.forEach((fileName) => {
+  if (!fileName.endsWith(".js")) return;
 
-	const file = require(`./components/${file_name}`);
+  const file = require(`./components/${fileName}`);
+  const componentName = fileName.split(".js").join("");
 
-	const component_name = file_name.split('.js').join('');
-
-	if (config.components[component_name])
-		components.set(component_name, file);
+  if (config.components[componentName]) {
+    components.set(componentName, file);
+  }
 });
 
+// Called every interval (seconds) to update the message with stats
 async function update() {
-	if (!MESSAGE) return console.log('Unable to fetch channel or message.');
+  if (!MESSAGE) return console.log("Unable to fetch message.");
 
-	let payload = '';
+  let payload = "";
 
-	const promises = [];
-	components.forEach(component => promises.push(component.update()));
+  const promises = [];
+  components.forEach((component) => promises.push(component.update()));
 
-	const values = await Promise.all(promises);
-	payload = values.join('\n');
+  const values = await Promise.all(promises);
+  payload = values.join("\n");
 
-	payload += `\n:timer: **Last Updated:** ${moment().format("hh:mm:ss A DD-MM-YYYY")} `;
-	MESSAGE.edit(payload);
-	setTimeout(update, config.interval * 1000);
+  payload += `\n:timer: **Last Updated:** ${moment().format(
+    "hh:mm:ss A DD-MM-YYYY"
+  )}`;
+  MESSAGE.edit(payload);
+  setTimeout(update, config.interval * 1000);
 }
 
-client.on('ready', async () => {
-	client.user.setPresence({ activity: { name: 'Watching s.help' }, status: 'active' })
-	console.log(`Logged in as ${client.user.tag} !`);
+client.on("ready", async () => {
+  client.user.setPresence({
+    activity: { name: "Watching s.help" },
+    status: "active",
+  });
+  console.log(`Logged in as ${client.user.tag} !`);
 
-	if (config.messageID && config.channelID) {
-		let channel = await client.channels.fetch(config.channelID);
-		MESSAGE = await channel.messages.fetch(config.messageID);
-		console.log(`Updating every ${config.interval}s...`);
-		update();
-	} else {
-		console.log("Waiting for s.start");
-	}
+  if (config.messageID && config.channelID) {
+    try {
+      const channel = await client.channels.fetch(config.channelID);
+      MESSAGE = await channel.messages.fetch(config.messageID);
+      console.log(`Updating every ${config.interval}s...`);
+      update();
+    } catch (err) {
+      console.log("Unable to fetch previous message.");
+      config.messageID = undefined;
+      config.channelID = undefined;
+      MESSAGE = undefined;
+      fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
+    }
+  } else {
+    console.log("Waiting for s.start");
+  }
 });
 
-client.on('message', async (message) => {
-	if (message.author.bot) return;
-	if (message.content === 's.start') {
-		if (config.messageID) return message.reply("stats has already started.");
+client.on("message", async (message) => {
+  if (message.author.bot) return;
 
-		let msg = await message.channel.send("Updatig stats...");
-		config.messageID = msg.id;
-		config.channelID = msg.channel.id;
-		MESSAGE = msg;
-		fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
-		update();
-	}
-	if (message.content === 's.ping') {
-		const msg = await message.channel.send('Ping?')
-		msg.edit(`Pong! Latency is ${Math.round(msg.createdTimestamp - message.createdTimestamp)} ms. API Latency is ${Math.round(client.ws.ping)} ms`)
-	}
-	if (message.content === 's.help') {
-		const help = [
-			'Welcome to PC STAT BOT',
-			'',
-			'Here are the list of commands: ',
-			'`s.ping` - shows my latency',
-			'`s.start` - start updating the stats',
-			'`s.help` - shows this message',
-			'',
-			'About me',
-			'I am made by Delano Lourenco - https://delano-lourenco.web.app',
-			'For suggestions or bugs join my support server - https://discord.gg/FZY9TqW'
-		].join('\n');
-		return message.channel.send(help);
-	}
+  if (message.content === "s.start") {
+    if (config.messageID) return message.reply("stats has already started.");
+
+    let msg = await message.channel.send("Updating stats...");
+    config.messageID = msg.id;
+    config.channelID = msg.channel.id;
+    MESSAGE = msg;
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
+    update();
+  } else if (message.content === "s.stop") {
+    if (!config.messageID) return message.reply("stats is not started.");
+
+    const msg = await message.reply("stopping stats...");
+    config.messageID = undefined;
+    config.channelID = undefined;
+    MESSAGE = undefined;
+    fs.writeFileSync("./config.json", JSON.stringify(config, null, 2));
+    msg.edit("Stats was stopped.");
+  } else if (message.content === "s.ping") {
+    const msg = await message.channel.send("Ping?");
+    msg.edit(
+      `Pong! Latency is ${Math.round(
+        msg.createdTimestamp - message.createdTimestamp
+      )} ms. API Latency is ${Math.round(client.ws.ping)} ms`
+    );
+  } else if (message.content === "s.help") {
+    const help = [
+      "Welcome to PC STAT BOT",
+      "",
+      "Here are the list of commands: ",
+      "`s.ping` - shows my latency",
+      "`s.start` - start updating the stats",
+      "`s.stop` - stop updating the stats",
+      "`s.help` - shows this message",
+      "",
+      "About me",
+      "I am made by Delano Lourenco - https://delano-lourenco.web.app",
+      "For suggestions or bugs join my support server - https://discord.gg/FZY9TqW",
+    ].join("\n");
+    return message.channel.send(help);
+  }
 });
 
 try {
-	console.log("Starting PC STAT BOT");
-	if (config.clientID) console.log(`Invite Link: https://discord.com/oauth2/authorize?client_id=${config.clientID}&scope=bot&permissions=8`)
+  console.log("Starting PC STAT BOT");
+  if (config.clientID)
+    console.log(
+      `Invite Link: https://discord.com/oauth2/authorize?client_id=${config.clientID}&scope=bot&permissions=8`
+    );
 
-	client.login(config.token);
+  client.login(config.token);
 } catch (err) {
-	console.log('Error logging in.', err);
-	client.login(config.token);
+  console.log("Error logging in.", err);
+  client.login(config.token);
 }
 client.on("error", (err) => {
-	console.log('Dicord Client Error:', err);
-	client.login(config.token);
-})
-process.on('unhandledRejection', err => { console.log('Caught Error:', err) })
+  console.log("Dicord Client Error:", err);
+  client.login(config.token);
+});
+process.on("unhandledRejection", (err) => {
+  console.log("Caught Error:", err);
+});
